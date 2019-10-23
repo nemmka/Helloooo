f281ea017cd128eb9ac327b69f739a26b41a0eda
nemmka <nastasyasma@gmail.com>
2019-10-23 22:11:03 +0500

    v2

--- 111.cpp
+++ 111.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 int main(){
-      cout<<"Hello, World!"<<endl;
- 
+   char str[100];
+   cout<<"¬ведите"<<endl;
+   cin>>str;
+   cout<<"Hello, World"<<endl;
    return 0;
 }
